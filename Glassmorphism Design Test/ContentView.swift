2e7836f49d9b171dd74fa67995067100e091e83c
Semih Cetin <semihcettin@gmail.com>
2022-08-31 18:25:34 +0300

    Initial Commit

---
+++ Glassmorphism Design Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glassmorphism Design Test
+//
+//  Created by Semih Cetin on 31.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glassmorphism Design Test/Glassmorphism_Design_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glassmorphism_Design_TestApp.swift
+//  Glassmorphism Design Test
+//
+//  Created by Semih Cetin on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Glassmorphism_Design_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
